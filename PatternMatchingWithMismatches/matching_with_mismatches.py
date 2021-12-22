c084150ee9dd4df4ab78cda6d5dbc934a3eac6db
Michael Ehrlichman <ehrlichman.m@gmail.com>
2021-12-22 14:27:23 -0800

    pattern matching with some number of allowable mismatches

--- PatternMatchingWithMismatches/matching_with_mismatches.py
+++ PatternMatchingWithMismatches/matching_with_mismatches.py
@@ -26,31 +26,40 @@ class HashedString:
             self.xkm_cache[mix][k] = pow(self.x, k, self.m[mix])
         return (self.h[mix][a+k] % self.m[mix] - self.xkm_cache[mix][k] * self.h[mix][a]%self.m[mix]) % self.m[mix]
 
-def check_equal(h1,a1,h2,a2,k):
+def check_equal(h1,h2,a1,a2,k):
     if h1.HashAK(a1,k,0) == h2.HashAK(a2,k,0):
         if h1.HashAK(a1,k,1) == h2.HashAK(a2,k,1):
             return True
     return False
+
+def check_and_split(h1,h2,a1,a2,k):
+    if k == 1:
+        if h1.s[a1]==h2.s[a2]:
+            ans = 0
+        else:
+            ans = 1
+        return ans
+    check = check_equal(h1,h2,a1,a2,k)
+    if check:
+        return 0
+    else:
+        k0 = k
+        k = k0//2
+        l = k0 - k0//2 
+        ans1 = check_and_split(h1,h2, a1,   a2,   k)
+        ans2 = check_and_split(h1,h2, a1+k, a2+k,  l)
+        return ans1 + ans2
 
 def solve(k, text, pattern):
     ht = HashedString(text)
     hp = HashedString(pattern)
     matches = []
     for i in range(ht.n - hp.n + 1):
-        k = hp.n
-        at = i
-        ap = 0
-        if check_equal(ht,at,hp,ap,k):
+        if check_and_split(ht,hp,i,0,hp.n) <= k:
             matches.append(i)
-        else:
-            while True:
-                l = k - k//2 
-                k = k//2
-                check_equal(ht,at,hp,0,k)
-                check_equal(ht,at+l,hp,0,l)
-	return matches
+    return matches
 
 for line in sys.stdin.readlines():
-	k, t, p = line.split()
-	ans = solve(int(k), t, p)
-	print(len(ans), *ans)
+    k, t, p = line.split()
+    ans = solve(int(k), t, p)
+    print(len(ans), *ans)
